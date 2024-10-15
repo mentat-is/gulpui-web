1e6f3150940316920c8e50728062fa55b0cb7867
Herasymchuk Mark <markgerasimchuk8@gmail.com>
2024-10-15 02:53:35 +0200

    🧠 Smart Ingestion | 3 / 6

--- src/app/gulp/components/header/DataTransfered.tsx
+++ src/app/gulp/components/header/DataTransfered.tsx
@@ -2,6 +2,7 @@ import { useApplication } from '@/context/Application.context'
 import { Button } from '@/ui/Button';
 import s from '../../Gulp.module.css';
 import { UploadBanner } from '@/banners/Upload.banner';
+import { formatBytes } from '@/ui/utils';
 
 export function DataTransfered() {
   const { app, spawnBanner } = useApplication();
@@ -21,14 +22,6 @@ interface UnitProps {
 }
 
 function Unit({ type, num }: UnitProps) {
-  const formatBytes = (bytes: number): string => {
-    if (bytes >= 1024 * 1024) {
-      return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
-    } else {
-      return `${(bytes / 1024).toFixed(2)} KB`;
-    }
-  };
-
   return (
     <Button variant='outline' img={type === 'upstream' ? 'CloudUpload' : 'CloudDownload'} className={s.unit}>
       {formatBytes(num)}
