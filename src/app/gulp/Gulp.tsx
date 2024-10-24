aad060a514ff1700c8e44dfff3ec0581c4dd0259
Herasymchuk Mark <markgerasimchuk8@gmail.com>
2024-10-24 13:33:18 +0200

    Menu Logic 1 / 3

--- src/app/gulp/Gulp.tsx
+++ src/app/gulp/Gulp.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Body } from './components/body/Body'
 import { DataTransfered } from './components/header/DataTransfered'
 import { Limits } from './components/header/Limits'
 import { Logout } from './components/header/Logout'
+import { Menu } from './components/header/Menu'
+import { Button } from '@/ui/Button'
 
 export function GulpPage() {
+  const [active, setActive] = useState(false);
 
   return (
     <React.Fragment>
       <header>
-        <DataTransfered />
+        <Menu active={active} />
+        <Button img='Menu' onClick={() => setActive(!active)}>Menu</Button>
         <Limits />
         <Logout />
       </header>
