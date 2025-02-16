f5d5988c74a20ea9bc58ea11466bb34e125df790
MiregX <markgerasimchuk8@gmail.com>
2025-02-16 01:52:35 +0100

    Help needed

--- src/engines/Default.engine.tsx
+++ src/engines/Default.engine.tsx
@@ -1,8 +1,8 @@
 import { λFile } from '@/dto/Dataset';
 import { Engine, Hardcode, MaxHeight, MinHeight, Scale } from '../class/Engine.dto';
 import { RenderEngine } from '../class/RenderEngine';
-import { Gradients, numericRepresentationOfAnyValueOnlyForInternalUsageOfRenderEngine, throwableByTimestamp, λColor } from '@/ui/utils';
-import { File } from '@/class/Info';
+import { getTimestamp, Gradients, numericRepresentationOfAnyValueOnlyForInternalUsageOfRenderEngine, throwableByTimestamp, λColor } from '@/ui/utils';
+import { File, MinMax } from '@/class/Info';
 
 export class DefaultEngine implements Engine.Interface<typeof DefaultEngine.target> {
   private static instance: DefaultEngine | null = null;
@@ -41,25 +41,52 @@ export class DefaultEngine implements Engine.Interface<typeof DefaultEngine.targ
     if (this.is(file) && !force) return this.map.get(file.id)! as typeof DefaultEngine.target;
 
     const map = new Map() as typeof DefaultEngine.target;
+    const events = File.events(this.renderer.info.app, file);
+    const { width } = this.renderer.ctx.canvas;
+    const pixels: MinMax = {
+      min: Math.max(this.renderer.getPixelPosition(file.timestamp.min), 0),
+      max: Math.min(this.renderer.getPixelPosition(file.timestamp.max), width),
+    }
+    console.log({ pixels });
 
-    File.events(this.renderer.info.app, file).forEach(event => {
-      const timestamp = event.timestamp + file.settings.offset as Hardcode.Timestamp;
-      const pos = this.renderer.getPixelPosition(timestamp) as Hardcode.X;
+    const mustHave = pixels.max - pixels.min;
 
-      if (map.has(pos))
-        return;
+    console.log({ mustHave });
 
-      const value = parseInt(event[file.settings.field].toString()) as Hardcode.Height;
+    for (let x = 0; x < mustHave; x++) {
+      let low = 0, high = events.length;
+      while (low < high) {
+        const mid = (low + high) >>> 1;
+        const eventTime = events[mid]?.timestamp + file.settings.offset;
+        
+        if (this.renderer.getPixelPosition(eventTime) < x) {
+          low = mid + 1;
+        } else {
+          high = mid;
+        }
+      }
 
-      map.set(pos, [value, timestamp]);
-    });
+      console.log(high, low);
 
-    const values = Array.from(map).map(v => v[1][0]);
+      if (low < events.length) {
+        const event = events[low];
+        const eventTime = event.timestamp + file.settings.offset;
+        const value = parseInt(event[file.settings.field].toString());
+        map.set(x as Hardcode.X, [
+          value as Hardcode.Height,
+          eventTime as Hardcode.Timestamp
+        ]);
+      }
+    }
+
+    if (file.name === 'testapache.log') {
+      console.log(map); // Размер 245 пикселей, больее 2 миллионов ивентов, после аглорисма size: 0;
+    }
 
     map[Scale] = this.renderer.info.app.timeline.scale as Hardcode.Scale;
     map[MinHeight] = file.code.min as Hardcode.Height;
     map[MaxHeight] = file.code.max as Hardcode.Height;
-    this.map.set(file.id, map)
+    this.map.set(file.id, map);
 
     return map as typeof DefaultEngine.target;
   };
