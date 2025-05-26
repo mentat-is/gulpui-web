1051043ed51ab900a2cd520da5ff1ca8d50da208
MiregX <markgerasimchuk8@gmail.com>
2025-05-26 12:49:11 +0200

    remove coconut.png

--- src/context/Extension.context.tsx
+++ src/context/Extension.context.tsx
@@ -1,10 +1,7 @@
-import { createContext, lazy, ReactNode, Suspense, useContext, useEffect, useState } from "react";
-import * as esbuild from 'esbuild-wasm';
+import { createContext, lazy, ReactNode, useContext, useEffect, useState } from "react";
 import { useApplication } from "./Application.context";
 import React from "react";
-import { GulpDataset } from "@/class/Info";
 import { Logger } from "@/dto/Logger.class";
-import { Skeleton } from "@impactium/components";
 
 const __component = Symbol('λ_extension_component');
 
@@ -18,25 +15,12 @@ const KNOWN_EXTENSIONS: Record<string, Pick<Extension.Interface, 'type'>> = {
 } as const;
 
 export function ExtensionProvider({ children }: Extension.Provider.Props) {
-  const { Info, app } = useApplication();
+  const { app } = useApplication();
   const { banner } = useApplication();
   const [extensions, setExtensions] = useState<Record<string, Extension.Interface>>({});
-  const [initialized, setInitialized] = useState<boolean>(false);
 
   useEffect(() => {
     const new_extensions: typeof extensions = {};
-    const plugins = Info.app.target.plugins.filter(p => p.tags.includes('extension'))
-    for (const index in plugins) {
-      const plugin = plugins[index];
-
-      remote(plugin).then(e => {
-        if (!e) {
-          return;
-        }
-
-        new_extensions[plugin.filename] = e;
-      });
-    }
 
     for (const plugin in KNOWN_EXTENSIONS) {
       const extension = KNOWN_EXTENSIONS[plugin];
@@ -55,70 +39,6 @@ export function ExtensionProvider({ children }: Extension.Provider.Props) {
 
     setExtensions(new_extensions);
   }, [app.target.plugins]);
-
-  const initialize = async () => {
-    if (initialized)
-      return;
-
-    await esbuild.initialize({
-      wasmURL: "/esbuild.wasm",
-      worker: true
-    })
-
-    setInitialized(true);
-  };
-
-  const parse = async (raw: string): Promise<React.LazyExoticComponent<React.ComponentType<any>>> => {
-    await initialize();
-
-    const { code } = await esbuild.transform(raw, {
-      loader: 'tsx',
-      jsx: "transform",
-      target: "es2017"
-    });
-
-    const blob = new Blob([code], { type: 'application/javascript' });
-
-    const url = URL.createObjectURL(blob);
-
-    return React.lazy(() => import(/* webpackIgnore: true */ url));
-  }
-
-  const remote = async (plugin: GulpDataset.PluginList.Interface): Promise<Extension.Interface | undefined> => {
-    const { type, content } = await api<{
-      type: Extension.Type[],
-      content: string,
-    }>(`/extension_chunk`, {
-      method: 'GET',
-      query: {
-        plugin: plugin.filename
-      }
-    });
-
-    const component = await parse(content);
-
-    return {
-      type,
-      [__component]: component,
-    };
-  }
-
-  // const local = async (name: string): Promise<Extension.Interface | undefined> => {
-  //   try {
-  //     const response2 = await fetch(`/plugins/${name}.tsx`);
-
-  //     const data2 = await response2.text();
-
-  //     const component = await parse(data2);
-
-  //     return {
-  //       type: ['menu'],
-  //       [__component]: component,
-  //     };
-  //   } catch (e: any) {
-  //     Logger.error(e);
-  //   }
-  // }
 
   const extensionProps: Extension.Export = {
     extensions
