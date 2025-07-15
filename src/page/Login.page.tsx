ffa64263fb126fb38782bbc976aeb5ade5d3b16f
MiregX <markgerasimchuk8@gmail.com>
2025-07-15 02:14:51 +0200

    Removed ASCII | Fix #57

--- src/page/Login.page.tsx
+++ src/page/Login.page.tsx
@@ -1,22 +1,10 @@
-import Matrix from "@/components/Matrix";
 import { Stack } from "@impactium/components";
 import s from './styles/LoginPage.module.css';
 import { Auth } from "@/page/Auth.page";
-import { ASCII } from '@/ui/ASCII';
 
 export function LoginPage() {
   return (
     <Stack dir='row' gap={0} className={s.wrapper}>
-      <Matrix
-        glitchColors={['#2b4539', '#00ff00', '#008000']}
-        glitchSpeed={50}
-        centerVignette={true}
-        outerVignette={true}
-        smooth={true} />
-      <ASCII
-        enableWaves={false}
-        text='gULP'
-      />
       <Auth.Page />
     </Stack>
   )
