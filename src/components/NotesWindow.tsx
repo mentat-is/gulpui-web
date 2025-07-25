8c2df3742b46dadd0dd951224005f54c7f733c0a
MiregX <markgerasimchuk8@gmail.com>
2025-07-25 17:18:59 +0200

    Update NotesWindow.tsx

--- src/components/NotesWindow.tsx
+++ src/components/NotesWindow.tsx
@@ -3,11 +3,12 @@ import s from './styles/NotesWindow.module.css'
 import { Banner } from '@/ui/Banner'
 import { λNote } from '@/dto/Dataset'
 import { NotePoint } from '@/ui/Note'
-import { useEffect, useState, useMemo, useRef } from 'react'
-import { Input } from '@impactium/components'
+import { useEffect, useState, useMemo, useRef, useCallback } from 'react'
+import { Input, Stack } from '@impactium/components'
 import { Context, File } from '@/class/Info'
 import { useVirtualizer } from '@tanstack/react-virtual'
 import { Notification } from '@/ui/Notification'
+import { Select } from '@/ui/Select'
 
 interface FloatingWindowProps {
   onClose: () => void
@@ -18,10 +19,32 @@ export function NotesWindow({ onClose }: FloatingWindowProps) {
   const { app } = useApplication()
   const [search, setSearch] = useState('');
 
+  const getAvailableTags = useCallback(() => {
+    const tags = new Set<string>();
+
+    app.target.notes.forEach(note => {
+      note.tags.forEach(tag => {
+        const lowerTag = tag.toLowerCase();
+        if (!tags.has(lowerTag)) {
+          tags.add(lowerTag);
+        }
+      })
+    })
+
+    return [...tags.values()];
+  }, [app.target.notes]);
+
+  const [availableTags, setAvailableTags] = useState(getAvailableTags());
+  const [selectedTags, setSelectedTags] = useState<Set<string>>(new Set());
+
+  useEffect(() => {
+    setAvailableTags(getAvailableTags());
+  }, [app.target.notes]);
+
   const parentRef = useRef<HTMLDivElement>(null)
 
   const sortedNotes = useMemo(() => {
-    const filtered = search.length ? app.target.notes.filter(n => {
+    const prefiltered = search.length ? app.target.notes.filter(n => {
       const searchLower = search.toLowerCase()
       return (
         n.name.toLowerCase().includes(searchLower) ||
@@ -32,10 +55,12 @@ export function NotesWindow({ onClose }: FloatingWindowProps) {
       )
     }) : app.target.notes;
 
+    const filtered = selectedTags.size ? prefiltered.filter(n => [...selectedTags].every(selectedTag => n.tags.some(tag => tag.toLowerCase() === selectedTag))) : prefiltered;
+
     return filtered
       .sort((a, b) => a.context_id.localeCompare(b.context_id))
       .sort((a, b) => a.source_id.localeCompare(b.source_id))
-  }, [app.target.notes, search, app]);
+  }, [app.target.notes, search, app, selectedTags]);
 
   const virtualizer = useVirtualizer({
     count: sortedNotes.length,
@@ -46,14 +71,27 @@ export function NotesWindow({ onClose }: FloatingWindowProps) {
 
   return (
     <Banner title="Notes" onClose={onClose} className={s.main}>
-      <Input
-        placeholder='Context name, source name, note title or note description'
-        img='MagnifyingGlass'
-        variant='highlighted'
-        value={search}
-        onChange={e => setSearch(e.target.value)}
-      />
-
+      <Stack>
+        <Input
+          placeholder='Context name, source name, note title or note description'
+          img='MagnifyingGlass'
+          variant='highlighted'
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <Select.Multi.Root value={[...selectedTags.values()]} onValueChange={values => setSelectedTags(new Set(values))}>
+          <Select.Trigger>
+            <Select.Multi.Value icon={['DataPointMedium', 'DataPoint']} placeholder='Select tags to be shown' text={len => typeof len === 'number' ? `Selected ${len} tags` : len} />
+          </Select.Trigger>
+          <Select.Content>
+            {availableTags.map(tag => (
+              <Select.Item value={tag}>
+                {tag}
+              </Select.Item>
+            ))}
+          </Select.Content>
+        </Select.Multi.Root>
+      </Stack>
       <div
         ref={parentRef}
         className={s.result}
