9ea8cd16343ea57957e97fbeaa6d152d9eb890f4
NpmRunInstall <151098313+Takemyeye@users.noreply.github.com>
2025-12-19 19:52:26 +0100

    #136

--- src/banners/Sigma.tsx
+++ src/banners/Sigma.tsx
@@ -1,17 +1,17 @@
 import { Application } from '@/context/Application.context'
-import { Banner as UIBanner } from '@/ui/Banner'
-import { NodeFile } from '@/ui/utils'
-import { ChangeEvent, useMemo, useState } from 'react'
-import { toast } from 'sonner'
 import { Extension } from '@/context/Extension.context'
+import { Banner as UIBanner } from '@/ui/Banner'
+import { ChangeEvent, useState } from 'react'
+import { Source } from '@/entities/Source'
 import { Checkbox } from '@/ui/Checkbox'
-import { Label } from '@/ui/Label'
 import { Icon } from '@impactium/icons'
+import { NodeFile } from '@/ui/utils'
+import { Button } from '@/ui/Button'
+import { Toggle } from '@/ui/Toggle'
+import { Label } from '@/ui/Label'
 import { Input } from '@/ui/Input'
 import { Stack } from '@/ui/Stack'
-import { Button } from '@/ui/Button'
-import { Source } from '@/entities/Source'
-import { Toggle } from '@/ui/Toggle'
+import { toast } from 'sonner'
 
 export namespace Sigma {
   export namespace Banner {
@@ -24,19 +24,19 @@ export namespace Sigma {
     sources: initialSources = [],
     ...props
   }: Sigma.Banner.Props) {
-    const { Info, app, destroyBanner, spawnBanner } = Application.use();
-    const [rules, setRules] = useState<NodeFile[] | null>();
-    const [sources, setSources] = useState(initialSources)
     const [createNotes, setCreateNotes] = useState<boolean>(false);
+    const [sources, setSources] = useState(initialSources)
+    const [rules, setRules] = useState<NodeFile[]>([]);
+    const [isZip, setIsZip] = useState<boolean>(false);
+    const { Info, destroyBanner } = Application.use();
 
     const DoneButton = () => {
       const submit = async () => {
-        if (!rules) {
+        if (!rules.length && !isZip) {
           return toast('Select at least one rule or zip file')
         }
 
         await Info.query_sigma(sources, rules, createNotes);
-
         destroyBanner()
       }
 
@@ -45,7 +45,6 @@ export namespace Sigma {
           icon="Check"
           onClick={submit}
           variant="glass"
-          disabled={!rules}
         />
       )
     }
@@ -53,8 +52,8 @@ export namespace Sigma {
     const rulesInputChangeHandler = async (
       event: ChangeEvent<HTMLInputElement>,
     ) => {
-      const { files: rules } = event.target
-      if (!rules?.length) {
+      const { files } = event.target
+      if (!files?.length) {
         toast.warning('No sigma rules selected', {
           richColors: true,
           icon: <Icon name='Warning' />
@@ -62,24 +61,23 @@ export namespace Sigma {
         return;
       }
 
-      const fileData: NodeFile[] = Array.from(rules);
-
+      const fileData: NodeFile[] = Array.from(files);
       setRules(fileData);
     }
 
     return (
       <UIBanner title="Apply sigma rules" done={<DoneButton />} {...props}>
         <Extension.Optional name='SigmaZip.banner.tsx'>
-          <Toggle option={['File', 'Zip']} checked={false} onClick={() => spawnBanner(<Extension.Component name='SigmaZip.banner.tsx' props={{ sources }} />)} />
+          <Toggle option={['File', 'Zip']} checked={isZip} onClick={() => setIsZip(!isZip)} />
         </Extension.Optional>
+
+        {isZip ? (
+          <Extension.Component name='SigmaZip.banner.tsx' props={{ sources }} />
+        ) : (
+          <Input type="file" icon="Sigma" multiple variant="highlighted" onChange={rulesInputChangeHandler}/>
+        )}
+
         <Source.Select.Multi selected={sources} setSelected={setSources} placeholder='Select sources to apply sigma rules for them' />
-        <Input
-          type="file"
-          icon="Sigma"
-          multiple
-          variant="highlighted"
-          onChange={rulesInputChangeHandler}
-        />
         <Stack ai='center' gap={6}>
           <Checkbox id='isCreateNotes' checked={createNotes} onCheckedChange={v => setCreateNotes(!!v)} />
           <Label htmlFor='isCreateNotes' cursor='pointer' value='Create notes' />
